39240e302d687ca07838050516bd304132bf35de
Peng Lu <lupeng_nwpu@qq.com>
2025-04-08 17:57:30 +0800

    t02

--- dev-support/docker/rocky8/make_ambari_image_rocky8.sh
+++ dev-support/docker/rocky8/make_ambari_image_rocky8.sh
@@ -17,13 +17,16 @@
 
 workdir=$(cd $(dirname $0); pwd)
 AMBARI_PROJECT_ROOT=$workdir/../../../
-AMBARI_VERSION=`awk -F '[<>]' '/<revision>/ {print $3; exit}' $AMBARI_PROJECT_ROOT/pom.xml`
+AMBARI_VERSION=`awk -F '[<>]' '/<revision>/ {print $3; exit}' ${AMBARI_PROJECT_ROOT}/pom.xml`
 
 echo "Building ambari on rocky8"
-sh ./build_ambari_rocky8.sh ${AMBARI_PROJECT_ROOT} ${AMBARI_VERSION}
+sh ${workdir}/build_ambari_rocky8.sh ${AMBARI_PROJECT_ROOT} ${AMBARI_VERSION}
 echo "Building ambari server on rocky8 successfully"
 
 echo "Building ambari image on rocky8"
+cd ${workdir}
 ambari_server_rpm_path=${AMBARI_PROJECT_ROOT}/ambari-server/target/rpm/ambari-server/RPMS/x86_64/ambari-server-${AMBARI_VERSION}.x86_64.rpm
-docker build -t ambari:${AMBARI_VERSION}-rocky-8 -f $workdir/Dockerfile --build-arg AMABARI_SERVER_RPM_PATH=$ambari_server_rpm_path
+cp ${ambari_server_rpm_path} ambari-server-${AMBARI_VERSION}.x86_64.rpm
+docker build -t ambari:${AMBARI_VERSION}-rocky-8 ${workdir}/ --build-arg AMABARI_SERVER_RPM=ambari-server-${AMBARI_VERSION}.x86_64.rpm
+rm -f ambari-server-${AMBARI_VERSION}.x86_64.rpm
 echo "Building ambari image on rocky8 successfully"
